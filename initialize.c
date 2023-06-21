a1e13b53e9bd5e09c23de4c1f5c704b0f71697c9
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-21 10:09:27 -0700

    created the mont function

---
+++ initialize.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * initialize - initialize parameters of param structure
+ */
+param *mont;
+void initialize(void)
+{
+	mont = malloc(sizeof(param));
+	if (!mont)
+		malloc_error();
+	mont->n = 0;
+	mont->line_number = 1;
+	mont->arr = NULL;
+}
