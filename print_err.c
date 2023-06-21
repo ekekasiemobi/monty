057fbf1685927e90b3573847cbe0ef5023f0b762
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-21 09:53:41 -0700

    created the error function

---
+++ print_err.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * print_error - Print error message to stderr
+ * @message: error message
+ * @file: exit status code
+ */
+void print_error(char *message, char *file)
+{
+	if (file == NULL)
+		file = "";
+	fprintf(stderr, "%s %s\n", message, file);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * malloc_error - print malloc error
+ */
+void malloc_error(void)
+{
+	fprintf(stderr, "Error: malloc failed");
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * print_line_number - print unknown error:
+ * @line: line number
+ */
+void print_line_number(unsigned int line)
+{
+	fprintf(stderr, "L %d: unknown instruction %s\n", line, mont->arr[0]);
+	exit(EXIT_FAILURE);
+}
