cb2424353d5ed43a9c2a71572d5679333a6d9cd3
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-21 09:57:10 -0700

    created the free array function

---
+++ free_array.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * free_array - free all memory allocated
+ */
+void free_array(void)
+{
+	int i = 0;
+	
+	for (; mont->arr[i] != NULL; i++)
+		free(mont->arr[i]);
+	free(mont->arr);
+	mont->arr = NULL;
+}
