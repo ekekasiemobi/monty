f5b5eae355c90045e0731a81e87711d69b92ca3e
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-21 09:55:05 -0700

    vreated the pop function

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * pop - a function that delete on a stack
+ * @head: a struct node pointer
+ */
+void pop(stack_t **stack, unsigned int line)
+{
+	stack_t *next_val;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L %d: can't pop an empty stack\n", line);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	next_val = (*stack)->next;
+	if (next_val->prev != NULL)
+		next_val->prev = NULL;
+	free(*stack);
+	*stack = next_val;
+}
