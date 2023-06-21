5ad3f3e61f265f6c08c6f78860f2f5a805a504e9
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-21 09:56:05 -0700

    created the free stack function

---
+++ free_stack.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ * free_stack - free stack
+ * @head: a struct node pointer
+ */
+void free_stack(stack_t **stack)
+{
+	stack_t *temp;
+
+	while (*stack != NULL)
+	{
+		temp = *stack;
+		*stack = (*stack)->next;
+		free(temp);
+		temp = NULL;
+	}
+}
