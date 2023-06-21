a0571109853fdb5c9e3e439af4c08f32f55fbe79
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-21 05:17:40 -0700

    created the add function

--- opcodes_functions.c
+++ opcodes_functions.c
@@ -23,7 +23,7 @@ void print_all(stack_t **stack, unsigned int line_count)
 /**
  * print_int - prints top item on the stack
  *
- * @stack: pointer to stacck to print
+ * @stack: pointer to stack to print
  * @line_count: number of lines counted
  */
 void print_int(stack_t **stack, unsigned int line_count)
@@ -35,6 +35,33 @@ void print_int(stack_t **stack, unsigned int line_count)
 	}
 	printf("%d\n", (*stack)->n);
 }
+
+/**
+ * print_add - adds two elements of the stack
+ *
+ * @stack: pointer to stack to print
+ * @line_count: number of lines in the stack
+ */
+void print_add(stack_t **stack, unsigned int line_count)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n",
+			line_count);
+		exit(EXIT_FAILURE);
+	}
+
+	new = *stack;
+
+	while (new->next != NULL)
+	{
+		new->next->n += new->n;
+		new = new->next;
+	}
+
+	free(new);
+}
+
 
 
 /**
