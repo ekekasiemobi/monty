4e3f8cd6a377cf9fa00070d6418609c9511ef214
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-22 03:30:55 -0700

    added the div prototype to the header file

--- monty.h
+++ monty.h
@@ -68,7 +68,7 @@ void print_int(stack_t **stack, unsigned int line_number);
 void print_all(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void print_add(stack_t **stack, unsigned int line_number);
-void print_div(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
 void print_mod(stack_t **stack, unsigned int line_number);
 void initialize(void);
 void print_line_number(unsigned int line);
