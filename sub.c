fb0f565d49830410007261404204faaf8ad9a66f
Rashnotech <rashnotech@gmail.com>
2023-06-22 10:00:14 +0000

    Fixed error message for sub

--- sub.c
+++ sub.c
@@ -11,7 +11,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	num = (*stack)->next->n;
